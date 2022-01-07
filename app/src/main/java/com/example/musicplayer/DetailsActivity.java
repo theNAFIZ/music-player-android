dea8d380614b199b05af8ab866c7eb6d4c077aad
theNAFIZ <nrpustcse@gmail.com>
2022-01-07 11:18:08 +0600

    Play button added in DetailsActivity. Music player is working only for hardcoded music file.

--- app/src/main/java/com/example/musicplayer/DetailsActivity.java
+++ app/src/main/java/com/example/musicplayer/DetailsActivity.java
@@ -2,13 +2,20 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
     private TextView name, description;
     private Bundle extras;
+
+    // TODO: pass dynamic files to play
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +30,39 @@ public class DetailsActivity extends AppCompatActivity {
         name.setText(extras.getString("name"));
         description.setText(extras.getString("description"));
 
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer =  MediaPlayer.create(getApplicationContext(), R.raw.whisle);
+
+        playButton = (Button) findViewById(R.id.playBtn);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    // pause music
+                    pauseMusic();
+                    playButton.setText("Play");
+
+                } else {
+                    // play music
+                    playMusic();
+                    playButton.setText("Pause");
+
+
+                }
+            }
+        });
+
+    }
+
+    public void pauseMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void playMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
     }
 }
